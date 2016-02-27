d6e4802b75ca30e5c373d0a850c3a92c241042da
Daniel Romero <daniel.romero.esteban@gmail.com>
2016-02-27 14:11:53 +0100

    ++

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # Serve websocket cable requests in-process
-<<<<<<< HEAD
   mount ActionCable.server => '/cable'
-=======
   # mount ActionCable.server => '/cable'
   root to: 'dashboard#index'
 
->>>>>>> origin/master
 end
