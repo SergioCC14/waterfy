d9a5f83b7104c2b8a82199ef3654878e6b49be90
Daniel Romero <daniel.romero.esteban@gmail.com>
2016-02-27 20:19:36 +0100

    probando...para heroku

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ require "rails/test_unit/railtie"
 
 Bundler.require(*Rails.groups)
 
-Dotenv::Railtie.load
+Dotenv::Railtie.load unless Rails.env.production?
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
