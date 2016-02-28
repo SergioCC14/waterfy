48849f6ba035b1ff39bea55b17b827cc7d3021b5
Daniel Romero <daniel.romero.esteban@gmail.com>
2016-02-28 11:58:47 +0100

    capistrano

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # waterfy
 The sea needs a revolution. We have the idea. The sea have water.
+
+# Server
+ec2-52-17-73-59.eu-west-1.compute.amazonaws.com
+
+# SSH
+ssh -i EC2Confisio.pem ubuntu@ec2-52-17-73-59.eu-west-1.compute.amazonaws.com
+
+## mongo
+  sudo service mongod start
+  sudo service mongod stop
+  sudo service mongod restart
+  https://docs.mongodb.org/manual/tutorial/install-mongodb-on-ubuntu/
+
+## redis
+  http://redis.io/topics/quickstart
---
+++ config/deploy.rb
@@ -0,0 +1,66 @@
+# config valid only for current version of Capistrano
+lock '3.4.0'
+
+set :application, 'waterfy'
+set :repo_url, 'git@github.com:SergioCC14/waterfy.git'
+
+# Default branch is :master
+# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+
+# Default deploy_to directory is /var/www/my_app_name
+# set :deploy_to, '/var/www/my_app_name'
+
+# Default value for :scm is :git
+# set :scm, :git
+
+# Default value for :format is :pretty
+# set :format, :pretty
+
+# Default value for :log_level is :debug
+# set :log_level, :debug
+
+# Default value for :pty is false
+# set :pty, true
+
+# Default value for :linked_files is []
+# set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+
+# Default value for linked_dirs is []
+# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for keep_releases is 5
+# set :keep_releases, 5
+
+set :rails_env, 'production'               # If the environment differs from the stage name
+set :migration_role, 'db'                  # Defaults to 'db'
+set :conditionally_migrate, true           # Defaults to false. If true, it's skip migration if files in db/migrate not modified
+set :assets_roles, [:web, :app]            # Defaults to [:web]
+set :assets_prefix, 'assets'   # Defaults to 'assets' this should match config.assets.prefix in your rails config/application.rb
+set :normalize_asset_timestamps, %{public/images public/javascripts public/stylesheets}
+
+# Default value for :linked_files is []
+#set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml', '.env')
+set :linked_files, %w{config/database.yml config/mongoid.yml config/secrets.yml .env}
+
+set(:config_files, %w(config/database.yml config/mongoid.yml config/secrets.yml .env))
+
+# Default value for linked_dirs is []
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+set :default_env, { rvm_bin_path: '~/.rvm/bin' }
+
+# Default value for keep_releases is 5
+# set :keep_releases, 5
+
+set :passenger_restart_with_touch, true
+
+
+# set the locations that we will look for changed assets to determine whether to precompile
+set :assets_dependencies, %w(app/assets/ lib/assets vendor/assets Gemfile config/routes.rb)
+
+class PrecompileRequired < StandardError; end
