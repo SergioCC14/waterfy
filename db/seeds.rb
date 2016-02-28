dfe71466fd2404a1d830d5f84a3cdda751a3c570
Daniel Romero <daniel.romero.esteban@gmail.com>
2016-02-28 12:51:11 +0100

    sensors seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,14 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-types = %w(d-left d-right gyroscope)
-types.each do |type|
-  Sensor.find_or_create_by(type: type)
+sensors = [
+  { type: 'accelerometer', udoo_id: '1' },
+  { type: 'gyroscope',     udoo_id: '2' },
+  { type: 'magnetometer',  udoo_id: '3' },
+  { type: 'horizontal',    udoo_id: '4' },
+  { type: 'horizontal',    udoo_id: '5' }
+  { type: 'horizontal',    udoo_id: '6' }
+]
+sensors.each do |sensor|
+  Sensor.find_or_create_by(type: sensor[:type], udoo_id: sensor[:udoo_id])
 end
